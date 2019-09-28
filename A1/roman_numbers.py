112b66311a67c69a685502c6a0bf993b4c649f79
Cragzu <cragzu@gmail.com>
2019-09-28 12:38:48 -0700

    Updated roman numbers file to include special numeral cases, added doctests

--- A1/roman_numbers.py
+++ A1/roman_numbers.py
@@ -10,10 +10,14 @@ def convert_to_roman_numeral(positive_int):
     Repeatedly divides the given int by increasingly smaller values to determine how many of each letter in the
     Roman number system to add to the roman_string.
 
-    :param positive_int: an int between 0 and 10000
     :precondition: positive_int must be an int greater than 0 and less than 10000
     :postcondition: calculate the equivalent to the given int in Roman Numerals
     :return: a string made up of capital letters from the Roman number system
+
+    >>> convert_to_roman_numeral(1444)
+    'MCDXLIV'
+    >>> convert_to_roman_numeral(999)
+    'MCMXCIX'
     """
     roman_string = ""  # initialize empty string
 
@@ -21,25 +25,55 @@ def convert_to_roman_numeral(positive_int):
     roman_string += ("M" * thousands)  # add that many Ms to the string
     positive_int -= (thousands * 1000)  # remove all thousands from the int
 
+    nine_hundreds = positive_int // 900  # special cases for certain roman numerals
+    if nine_hundreds != 0:
+        roman_string += "CM"
+        positive_int -= 900
+
     five_hundreds = positive_int // 500  # repeat the above steps for every roman letter
     roman_string += ("D" * five_hundreds)
     positive_int -= (five_hundreds * 500)
+
+    four_hundreds = positive_int // 400
+    if four_hundreds != 0:
+        roman_string += "CD"
+        positive_int -= 400
 
     hundreds = positive_int // 100
     roman_string += ("C" * hundreds)
     positive_int -= (hundreds * 100)
 
+    nineties = positive_int // 90
+    if nineties != 0:
+        roman_string += "XC"
+        positive_int -= 90
+
     fifties = positive_int // 50
     roman_string += ("L" * fifties)
     positive_int -= (fifties * 50)
+
+    forties = positive_int // 40
+    if forties != 0:
+        roman_string += "XL"
+        positive_int -= 40
 
     tens = positive_int // 10
     roman_string += ("X" * tens)
     positive_int -= (tens * 10)
 
+    nines = positive_int // 9
+    if nines != 0:
+        roman_string += "IX"
+        positive_int -= 9
+
     fives = positive_int // 5
     roman_string += ("V" * fives)
     positive_int -= (fives * 5)
+
+    fours = positive_int // 4
+    if fours != 0:
+        roman_string += "IV"
+        positive_int -= 4
 
     roman_string += ("I" * positive_int)  # ones don't need any further calculations
 
@@ -52,7 +86,7 @@ def main():
 
     Tests the function in this module.
     """
-    int_to_convert = int(input("Enter a positive int to convert, 1 <= int <= 10000: "))
+    int_to_convert = int(input("Enter a positive int to convert, 1 <= int <= 10 000: "))
 
     print(convert_to_roman_numeral(int_to_convert))
 
