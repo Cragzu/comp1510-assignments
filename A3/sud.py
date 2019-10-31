bbca17b604836ff4e2df559e0b7dc9d2653bf20f
Cragzu <cragzu@gmail.com>
2019-10-31 15:46:40 -0700

    Main module initial commit

---
+++ A3/sud.py
@@ -0,0 +1,15 @@
+"""
+Main module that runs the Single User Dungeon.
+"""
+# todo: imports go here
+
+
+def main():
+    """
+    Drive the SUD program.
+    """
+    # todo: program runner
+
+
+if __name__ == "__main__":
+    main()
