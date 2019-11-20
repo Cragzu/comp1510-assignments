9a55fb9db1ac459cab142c7dc27fb352f0aa1db8
Chloe (Desktop) <cragzu@gmail.com>
2019-11-19 21:21:44 -0800

    Created unit tests for cash_money function

--- A4/test_cash_money.py
+++ A4/test_cash_money.py
@@ -5,4 +5,10 @@ from question_5 import cash_money
 class TestCashMoney(TestCase):
 
     def test_amount(self):
-        self.fail()
+        self.assertEqual(cash_money(66.53), {50: 1, 5: 3, 1: 1, 0.25: 2, 0.01: 3})
+
+    def test_empty(self):
+        self.assertEqual(cash_money(0.0), {})
+
+    # def test_invalid_argument(self):  # todo: enable when raising exception is implemented
+    #     self.assertRaises(Exception, cash_money, 'a')
