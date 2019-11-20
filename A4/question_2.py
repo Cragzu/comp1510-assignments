5fdf5b8601da1d46c83241513e2e0fe14cd79c9b
Chloe (Desktop) <cragzu@gmail.com>
2019-11-19 20:04:46 -0800

    Created GCD function

--- A4/question_2.py
+++ A4/question_2.py
@@ -3,6 +3,25 @@ Part 2 of 8: Module containing an algorithm for finding the greatest common divi
 """
 
 
+def gcd(a: int, b: int) -> int:  # todo: exception for improper argument (not a non-zero int)
+    """
+    Determine the greatest common divisor of the two given ints a and b.
+
+    Checks if b is 0, returns a if true. a will be the GCD.
+    If b is not 0, calls itself with the remainder of a/b and b. Continues until b is 0.
+
+    :param a: int
+    :param b: int
+    :precondition: a and b must be non-zero ints
+    :return: an int representing the GCD of a and b.
+
+    >>>gcd(270, 192)
+    6
+    """
+    if b == 0:  # check to break out of recursion
+        return a  # the non-zero value; the GCD
+
+    return gcd(b, a % b)  # standard recursion, get remainder of a/b and continue with b and the remainder
 
 
 def main():
@@ -11,6 +30,7 @@ def main():
 
     Showcases the function defined in this module.
     """
+    print(gcd(-25, 15))
 
 
 if __name__ == "__main__":
