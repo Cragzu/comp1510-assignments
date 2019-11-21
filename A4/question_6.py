054b4d504d829188488157dcac15f999682fd250
Cragzu <cragzu@gmail.com>
2019-11-21 11:09:08 -0800

    Added test examples to question 6

--- A4/question_6.py
+++ A4/question_6.py
@@ -1,11 +1,11 @@
 """
 Six Tests for Hypothetical Function line_intersect(l1, l2)
 
-l1 and l2 are lists containing 2 floats, representing a line with 2 points on a grid. Function will check whether
-the two lines are parallel, intersecting (and the point of intersection) or coincident.
+l1 and l2 are lists containing 2 lists containing 2 floats, representing a line with 2 points on a grid.
+Function will check whether the two lines are parallel, intersecting (and the point of intersection) or coincident.
 
 1. two intersecting lines
-
+line_intersect([[-1.0, 2.0], [-3.0, -2.0]], [[4.0, 1.0], [2.0, -3.0]])
 
 2. two parallel lines
 line_intersect([[-1.0, 2.0], [-3.0, -2.0]], [[1.0, 1.0], [-1.0, -3.0]])
@@ -14,9 +14,11 @@ line_intersect([[-1.0, 2.0], [-3.0, -2.0]], [[1.0, 1.0], [-1.0, -3.0]])
 line_intersect([[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]])
 
 4. lines that share one value (that will be the point of intersection)
+line_intersect([[-1.0, 2.0], [-3.0, -2.0]], [[4.0, 1.0], [-3.0, -2.0]])
 
-5. lines with two equal points (dots)
+5. lines with two equal points (dots, should return same as parallel)
+line_intersect([[-1.0, 2.0], [-1.0, 2.0]], [[4.0, 1.0], [4.0, 1.0]])
 
-6. negative numbers
-
+6. invalid input, should error
+line_intersect('a', 'b')
 """
