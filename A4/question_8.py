e44c10022b7a866b6903b6b320b81da7e6fc705e
Chloe (Desktop) <cragzu@gmail.com>
2019-11-22 20:35:52 -0800

    Created clock functions

---
+++ A4/question_8.py
@@ -0,0 +1,68 @@
+"""
+Part 8 of 8: Module containing an algorithm to find the time that
+displays the largest number of bars on a digital clock.
+"""
+
+
+def find_highest_bars(lowerbound: int, upperbound: int, padding=None) -> int:
+    """
+    Find the number requiring the highest number of bars to represent on a digital clock between the given parameters.
+
+    Loop through the numbers. Optionally pad the number with a leading padding, convert to string. Check the digits
+    against the amounts_of_bars dict to find the number requiring the most bars.
+
+    :param lowerbound:
+    :param upperbound:
+    :param padding:
+    :return:
+    """
+    # number: amount of bars
+    amounts_of_bars = {'0': 6, '1': 2, '2': 5, '3': 5, '4': 4, '5': 5, '6': 6, '7': 3, '8': 7, '9': 6}
+
+    greatest_amount = 0
+    most_bars = 0
+    current_amount = 0
+
+    for number in range(lowerbound, upperbound):
+
+        num_string = str(number)
+
+        if padding:  # pad with leading number if given
+            if len(num_string) == 1:
+                num_string = padding + num_string
+
+        for digit in num_string:
+            current_amount += amounts_of_bars[digit]
+
+        if current_amount > greatest_amount:
+            greatest_amount = current_amount
+            most_bars = num_string
+
+        current_amount = 0
+
+    return [most_bars, greatest_amount]
+
+
+def im_not_sleepy():
+
+    hours = find_highest_bars(1, 13)  # check 1-12
+    minutes = find_highest_bars(0, 60, '0')  # check 0-59, pad with extra 0
+
+    output = 'The greatest amount of bars on a clock is at the time ' + hours[0] + ':' + minutes[0]\
+             + ' with ' + str(hours[1] + minutes[1]) + ' bars.'
+
+    return output
+
+
+def main():
+    """
+    Drive the program.
+
+    Showcases the function defined in this module.
+    """
+    print(im_not_sleepy())
+
+
+
+if __name__ == "__main__":
+    main()
