74e7a9005d508ebc7b3ee89295c976b41a0e0b8a
Chloe (Desktop) <cragzu@gmail.com>
2019-11-22 20:40:46 -0800

    Created docstrings/doctests

--- A4/question_8.py
+++ A4/question_8.py
@@ -4,17 +4,19 @@ displays the largest number of bars on a digital clock.
 """
 
 
-def find_highest_bars(lowerbound: int, upperbound: int, padding=None) -> int:
+def find_highest_bars(lowerbound: int, upperbound: int, padding=None) -> list:
     """
     Find the number requiring the highest number of bars to represent on a digital clock between the given parameters.
 
     Loop through the numbers. Optionally pad the number with a leading padding, convert to string. Check the digits
     against the amounts_of_bars dict to find the number requiring the most bars.
 
-    :param lowerbound:
-    :param upperbound:
-    :param padding:
-    :return:
+    :param lowerbound: int
+    :param upperbound: int
+    :param padding: optional, a string to pad the 1-digit numbers with. Likely 0.
+    :precondition: padding must be a number 0-9
+    :precondition: lowerbound must be less than upperbound, they must both be ints
+    :return: a list containing the number and its required number of bars
     """
     # number: amount of bars
     amounts_of_bars = {'0': 6, '1': 2, '2': 5, '3': 5, '4': 4, '5': 5, '6': 6, '7': 3, '8': 7, '9': 6}
@@ -43,7 +45,13 @@ def find_highest_bars(lowerbound: int, upperbound: int, padding=None) -> int:
     return [most_bars, greatest_amount]
 
 
-def im_not_sleepy():
+def im_not_sleepy() -> str:
+    """
+    Finds the time on a digital clock that takes the most bars to represent.
+
+    :precondition: find_highest_bars must work as expected
+    :return: a string displaying the found time and its number of bars
+    """
 
     hours = find_highest_bars(1, 13)  # check 1-12
     minutes = find_highest_bars(0, 60, '0')  # check 0-59, pad with extra 0
@@ -61,7 +69,6 @@ def main():
     Showcases the function defined in this module.
     """
     print(im_not_sleepy())
-
 
 
 if __name__ == "__main__":
